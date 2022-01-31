1190c154229519264fd0c3bde2a4f696fc2b5ff6
zhiayang <zhiayang@nascentnoodle.com>
2022-02-01 00:06:13 +0800

    fix compile error on linux

---
+++ source/include/sap/layout.h
@@ -0,0 +1,6 @@
+// layout.h
+// Copyright (c) 2022, zhiayang
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
--- source/pdf/font/serialise.cpp
+++ source/pdf/font/serialise.cpp
@@ -2,6 +2,8 @@
 // Copyright (c) 2022, zhiayang
 // SPDX-License-Identifier: Apache-2.0
 
+#include <algorithm>
+
 #include "pdf/font.h"
 #include "pdf/misc.h"
 #include "pdf/object.h"
