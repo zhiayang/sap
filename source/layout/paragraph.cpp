9a6cfdc100354b70b18c69e4b02360a803a86f47
gracefu <81774659+gracefuu@users.noreply.github.com>
2022-12-23 22:58:54 +0800

    linebreak with dijk

--- source/layout/paragraph.cpp
+++ source/layout/paragraph.cpp
@@ -5,6 +5,8 @@
 #include "sap.h"
 
 #include "util.h"
+#include "dijkstra.h"
+
 #include "pdf/page.h"
 #include "pdf/font.h"
 #include "pdf/text.h"
@@ -12,6 +14,100 @@
 
 namespace sap::layout
 {
+
+	using WordVecIter = std::vector<Word>::const_iterator;
+
+	struct Line
+	{
+		std::vector<Word> words;
+		bool is_hyphenated = false;
+	};
+
+	static std::vector<WordVecIter> break_lines(const std::vector<Word>& words, Scalar preferred_line_length)
+	{
+		struct LineBreakNode
+		{
+			const std::vector<Word>* words;
+			Scalar preferred_line_length;
+			WordVecIter broken_until;
+			WordVecIter end;
+
+			using Distance = double;
+
+			std::vector<std::pair<LineBreakNode, Distance>> neighbours()
+			{
+				Scalar cur_line_length;
+				std::vector<std::pair<LineBreakNode, Distance>> ret;
+
+				WordVecIter it = broken_until;
+
+				// TODO: Handle lines that only consist of 1 word
+				//       This involves tweaking the cost computation
+				// TODO: Handle splitting a singular very long word into multiple words forcefully
+				if(std::distance(it, end) < 2)
+				{
+					sap::internal_error("Kill die me now because not yet implemented");
+				}
+
+				// Consume 2 words because there are more than 2 words remaining
+				cur_line_length += it->size.x();
+				++it;
+				auto prev_space = it->spaceWidth();
+				cur_line_length += it->size.x();
+				++it;
+
+				while(true)
+				{
+					LineBreakNode node { words, preferred_line_length, it, end };
+
+					// Cost calculation:
+					// for now, we just do square of the diff between
+					//   preferred space length (cur_line_length / (words - 1))
+					//   and actual space length (preferred_line_length / (words - 1)
+					//
+					// special case for last line: no cost!
+					if(it == end)
+					{
+						ret.emplace_back(node, Distance {});
+						break;
+					}
+					auto pref_space_len = cur_line_length.value() / static_cast<double>(it - broken_until - 1);
+					auto actual_space_len = preferred_line_length.value() / static_cast<double>(it - broken_until - 1);
+					ret.emplace_back(node, (pref_space_len - actual_space_len) * (pref_space_len - actual_space_len));
+
+					// Consume a word
+					cur_line_length += it->size.x();
+					auto next_space = it->spaceWidth();
+					auto space_between = dim::max(prev_space, next_space);
+					cur_line_length += space_between;
+					prev_space = next_space;
+					++it;
+
+					// Do not consider this word if we're too long
+					if(cur_line_length > preferred_line_length)
+					{
+						break;
+					}
+				}
+
+				return ret;
+			}
+
+			size_t hash() const { return static_cast<size_t>(broken_until - words->begin()); }
+			bool operator==(LineBreakNode other) const { return broken_until == other.broken_until; }
+		};
+
+		/* auto path = util::dijkstra_shortest_path(LineBreakNode { &words, preferred_line_length, words.begin(), words.end() },
+		 */
+		std::vector<LineBreakNode> path {};
+		std::vector<WordVecIter> path_as_iters;
+		for(const LineBreakNode& node : path)
+		{
+			path_as_iters.push_back(node.broken_until);
+		}
+		return path_as_iters;
+	}
+
 	void Paragraph::add(Word word)
 	{
 		word.m_paragraph = this;
